6d9b1bcdb4aa476c42d0539d8cb2981ced952dd2
vsternbach <vsternbach@paloaltonetworks.com>
2023-10-30 02:23:33 +0200

    fix: fix data parsing

--- parser/AccessoryParser.js
+++ parser/AccessoryParser.js
@@ -2,14 +2,14 @@ class AccessoryParser {
     constructor(platform, accessoryType) {
         this.platform = platform;
         this.accessoryType = accessoryType;
-        
+
         this.Accessory = platform.Accessory;
         this.PlatformAccessory = platform.PlatformAccessory;
         this.Service = platform.Service;
         this.Characteristic = platform.Characteristic;
         this.UUIDGen = platform.UUIDGen;
     }
-    
+
     getAccessoryUUID(deviceSid, accessoryType) {
         var result = null;
         switch(arguments.length) {
@@ -22,27 +22,27 @@ class AccessoryParser {
         }
         return result;
     }
-    
+
     getAccessoryCategory() {
         return null;
     }
-    
+
     getAccessoryInformation(deviceSid) {
         return {};
     }
-    
+
     getServices(jsonObj, accessoryName) {
         return [];
     }
-    
+
     getCreateAccessories(jsonObj) {
         var that = this;
         var deviceSid = jsonObj['sid'];
-        
+
         if(that.platform.ConfigUtil.getAccessoryDisable(deviceSid, that.accessoryType)) {
             return null;
         }
-        
+
         var uuid = that.getAccessoryUUID(deviceSid);
         var accessory = that.platform.AccessoryUtil.getByUUID(uuid);
         if(null == accessory) {
@@ -56,7 +56,7 @@ class AccessoryParser {
             that.getServices(jsonObj, accessoryName).forEach(function(service, index, serviceArr) {
                 accessory.addService(service, accessoryName);
             });
-            
+
             accessory.reachable = true;
             accessory.on('identify', function(paired, callback) {
                 that.platform.log.debug(accessory.displayName + " Identify!!!");
@@ -65,16 +65,17 @@ class AccessoryParser {
 
             return accessory;
         }
-        
+
         return null;
     }
-    
+
     parserAccessories(jsonObj) {
     }
-    
+
     getValueFrJsonObjData1(jsonObj, valueKey) {
         var dataStr = jsonObj['data'];
-        if(undefined != dataStr && null != dataStr) {
+        if (typeof dataStr !== "string") dataStr = JSON.stringify(dataStr);
+        if (undefined != dataStr && null != dataStr) {
             var dataObj = JSON.parse(dataStr);
             if(undefined != dataObj && null != dataObj) {
                 var value = dataObj[valueKey];
@@ -86,7 +87,7 @@ class AccessoryParser {
 
         return null;
     }
-    
+
     getValueFrJsonObjData2(jsonObj, valueKey) {
         var params = jsonObj['params'];
         if(undefined != params && null != params) {
@@ -100,7 +101,7 @@ class AccessoryParser {
 
         return null;
     }
-    
+
     getValueFrJsonObjData(jsonObj, valueKey) {
         var dataStr = jsonObj['data'];
         var params = jsonObj['params'];
@@ -112,7 +113,7 @@ class AccessoryParser {
             return null;
         }
     }
-    
+
     getLowBatteryByVoltage(voltage) {
         return isNaN(voltage) ? NaN : (voltage >= 2801 ? 0 : 1);
     }
@@ -120,7 +121,7 @@ class AccessoryParser {
     getBatteryLevelByVoltage(voltage) {
         return isNaN(voltage) ? NaN : ((voltage - 2800)/5);
     }
-    
+
     getStatusLowBatteryCharacteristicValue(jsonObj, defaultValue) {
         var proto_version_prefix = this.platform.getProtoVersionPrefixByProtoVersion(this.platform.getDeviceProtoVersionBySid(jsonObj['sid']));
         if(1 == proto_version_prefix) {
@@ -129,10 +130,10 @@ class AccessoryParser {
             var value = this.getValueFrJsonObjData2(jsonObj, 'battery_voltage');
         } else {
         }
-        
+
         return value ? this.getLowBatteryByVoltage(value / 1.0) : defaultValue;
     }
-    
+
     getBatteryLevelCharacteristicValue(jsonObj, defaultValue) {
         var proto_version_prefix = this.platform.getProtoVersionPrefixByProtoVersion(this.platform.getDeviceProtoVersionBySid(jsonObj['sid']));
         if(1 == proto_version_prefix) {
@@ -141,10 +142,10 @@ class AccessoryParser {
             var value = this.getValueFrJsonObjData2(jsonObj, 'battery_voltage');
         } else {
         }
-        
+
         return value ? this.getBatteryLevelByVoltage(value / 1.0) : defaultValue;
     }
-    
+
     parserBatteryService(accessory, jsonObj) {
         var that = this;
         var deviceSid = jsonObj['sid'];
@@ -161,7 +162,7 @@ class AccessoryParser {
         if(null != batteryLevelValue) {
             batteryLevelCharacteristic.updateValue(batteryLevelValue);
         }
-        
+
 //        if (batteryLevelCharacteristic.listeners('get').length == 0) {
 //            batteryLevelCharacteristic.on("get", function(callback) {
 //                var command = '{"cmd":"read", "sid":"' + deviceSid + '"}';
@@ -183,7 +184,7 @@ class AccessoryParser {
 //            });
 //        }
     }
-    
+
     callback2HB(deviceSid, characteristic, callback, err) {
         var that = this;
         if(err) {
